56ee43a63c6f51630a1b39cef4a5c8059d98c34b
micro-blue <56332575+micro-blue@users.noreply.github.com>
2021-03-29 22:43:32 +0800

    Add files via upload

---
+++ pstree/src/main/java/notifier/PartValCalculator.java
@@ -0,0 +1,34 @@
+package notifier;
+
+/**
+ * @author hjk
+ * @Description
+ * @date 2021/3/29 16:48
+ */
+public class PartValCalculator extends Notifier {
+    @Override
+    public void receiveFromParent(Long val) {
+
+    }
+
+    @Override
+    public void receiveFromChild( Long val1,Long val2) {
+
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+}
